8507ce33664a98ccdaaed4335f629a0b961797f4
StanRuaW <457354109@qq.com>
2020-08-15 15:00:51 +0800

    要做高清渲染管线之前保存一下，顺便调整了下代码小结构

--- Assets/Assets/Script/GunPrototype.cs
+++ Assets/Assets/Script/GunPrototype.cs
@@ -43,7 +43,7 @@ public class GunPrototype : VRTK_InteractableObject
         ResetSnapPoint();
     }*/
 
-    public void Shot()
+     public void Shot()
     {
         if (!HasBullet())
         {
